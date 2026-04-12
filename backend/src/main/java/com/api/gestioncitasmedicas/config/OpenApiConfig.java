4d217eacab7c551ad16ff7cb6d12d24ab4791080
RM24001-Reyes <rm24001@ues.edu.sv>
2026-04-12 00:15:52 -0600

    Se agrega documentacion Swagger

---
+++ backend/src/main/java/com/api/gestioncitasmedicas/config/OpenApiConfig.java
@@ -0,0 +1,20 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package com.api.gestioncitasmedicas.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return (new OpenAPI()).info((new Info()).title("API de Gestión de Citas Médicas").version("1.0.0").description("Sistema para la administración de citas médicas.").contact((new Contact()).name("DAW-Grupo1").email("mo21016@ues.edu.sv")));
+    }
+}
